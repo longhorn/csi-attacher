7f3bc582d027723d10971a2bed0629661d8d27d5
Derek Su <derek.su@suse.com>
2026-09-01 11:09:17 +0800

    chore: add GitHub Actions workflow for repository sync
    Signed-off-by: Derek Su <derek.su@suse.com>

--- vendor/go.opentelemetry.io/otel/sdk/trace/batch_span_processor.go
+++ vendor/go.opentelemetry.io/otel/sdk/trace/batch_span_processor.go
@@ -1,12 +1,11 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package trace
+package trace // import "go.opentelemetry.io/otel/sdk/trace"
 
 import (
 	"context"
 	"errors"
-	"fmt"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -164,21 +163,19 @@ func (bsp *batchSpanProcessor) Shutdown(ctx context.Context) error {
 	bsp.stopOnce.Do(func() {
 		bsp.stopped.Store(true)
 		wait := make(chan struct{})
-		// exportErr is written by the goroutine before closing wait.
-		// It is only read in the <-wait case, so there is no race.
-		var exportErr error
 		go func() {
 			close(bsp.stopCh)
 			bsp.stopWait.Wait()
 			if bsp.e != nil {
-				exportErr = bsp.e.Shutdown(ctx)
+				if err := bsp.e.Shutdown(ctx); err != nil {
+					otel.Handle(err)
+				}
 			}
 			close(wait)
 		}()
-		// Wait until the channel is ready or the context is canceled.
+		// Wait until the wait group is done or the context is cancelled
 		select {
 		case <-wait:
-			err = exportErr
 		case <-ctx.Done():
 			err = ctx.Err()
 		}
@@ -436,11 +433,11 @@ func (bsp *batchSpanProcessor) enqueueDrop(ctx context.Context, sd ReadOnlySpan)
 func (bsp *batchSpanProcessor) MarshalLog() any {
 	return struct {
 		Type         string
-		SpanExporter string
+		SpanExporter SpanExporter
 		Config       BatchSpanProcessorOptions
 	}{
 		Type:         "BatchSpanProcessor",
-		SpanExporter: fmt.Sprintf("%T", bsp.e),
+		SpanExporter: bsp.e,
 		Config:       bsp.o,
 	}
 }
